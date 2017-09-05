0188aac667553e13230ebb1377038d3b4ac495e9
wunnalwin26 <mr.wunnalwin91@gmail.com>
2017-09-05 17:13:56 +0630

    Simple Web Server nodejs

---
+++ server.js
@@ -0,0 +1,45 @@
+const http = require('http')
+const url = require('url')
+const path = require('path')
+const fs = require('fs')
+
+const mineTypes = {
+  "html": "text/html",
+  "jpeg": "image/jpeg",
+  "jpg": "image/jpg",
+  "png": "image/png",
+  "js": "text/javascript",
+  "css": "text/css"
+};
+
+http.createServer(function(req,res){
+  var uri = url.parse(req.url).pathname;
+  var fileName = path.join(process.cwd(), unescape(uri))
+  console.log('loading ' + uri)
+  var status;
+
+  try{
+    status = fs.lstatSync(fileName)
+  }catch(e){
+    res.writeHead(404, {'Content-type':'text/plain'});
+    res.write('404 Not Found\n')
+    res.end()
+    return;
+  }
+
+  if(status.isFile()){
+    var mineType = mineTypes[path.extname(fileName).split('.').reverse()[0]]
+    res.writeHead(200, {'Content-type':mineType})
+
+    var fileStream = fs.createReadStream(fileName)
+    fileStream.pipe(res)
+  }else if(status.isDirectory()){
+    res.writehead(302, {'Location':'index.html'})
+    res.end();
+  }else {
+    res.writeHead(500, {'Content-type':'text/plain'})
+    res.write('500 Internal Error\n')
+    res.end()
+  }
+
+}).listen(1337);
